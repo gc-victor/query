6935272890e7c9a8b22e835bc4c01096d7a2264c
Víctor García <gc.victor@gmail.com>
2024-01-11 13:32:42 +0100

    fix(server): add missing sqlite params

--- crates/server/src/controllers/functions/ext/init_sqlite.rs
+++ crates/server/src/controllers/functions/ext/init_sqlite.rs
@@ -2,20 +2,40 @@ use std::env;
 
 use anyhow::{anyhow, Result};
 use rustyscript::deno_core::{self, extension, op2};
+use serde_json::Value;
 
+use crate::controllers::utils::bind_to_params::{bind_array_to_params, bind_object_to_params};
 use crate::controllers::utils::query_to_json::query_to_json;
 use crate::sqlite::connect_db::connect_db;
 
 #[op2]
 #[string]
-fn op_sqlite_query_extension(#[string] db_name: String, #[string] query: String) -> Result<String> {
+fn op_sqlite_query_extension(
+    #[string] db_name: String,
+    #[string] query: String,
+    #[string] params: String,
+) -> Result<String> {
     let connection = connect_db(&db_name)?;
 
-    let statement = connection.prepare(&query);
+    let values: Value = serde_json::from_str(&params).unwrap();
+
+    let (statement, params) = if values.is_object() {
+        let (params, updated_query) = bind_object_to_params(values, query.to_owned())?;
+        let statement = connection.prepare(&updated_query);
+
+        (statement, params)
+    } else {
+        let params = bind_array_to_params(values);
+        let statement = connection.prepare(&query);
+
+        (statement, params)
+    };
 
     match statement.is_err() {
         true => Err(anyhow!(format!("Error: {}", statement.unwrap_err()))),
-        false => Ok(query_to_json(statement.unwrap(), []).unwrap().to_string()),
+        false => Ok(query_to_json(statement.unwrap(), params)
+            .unwrap()
+            .to_string()),
     }
 }
 
@@ -24,14 +44,29 @@ fn op_sqlite_query_extension(#[string] db_name: String, #[string] query: String)
 fn op_sqlite_execute_extension(
     #[string] db_name: String,
     #[string] query: String,
+    #[string] params: String,
 ) -> Result<String> {
     let connection = connect_db(&db_name)?;
 
-    let statement = connection.prepare(&query);
+    let values: Value = serde_json::from_str(&params).unwrap();
+
+    let (statement, params) = if values.is_object() {
+        let (params, updated_query) = bind_object_to_params(values, query.to_owned())?;
+        let statement = connection.prepare(&updated_query);
+
+        (statement, params)
+    } else {
+        let params = bind_array_to_params(values);
+        let statement = connection.prepare(&query);
+
+        (statement, params)
+    };
 
     match statement.is_err() {
         true => Err(anyhow!(format!("Error: {}", statement.unwrap_err()))),
-        false => Ok(query_to_json(statement.unwrap(), []).unwrap().to_string()),
+        false => Ok(query_to_json(statement.unwrap(), params)
+            .unwrap()
+            .to_string()),
     }
 }
 
--- crates/server/src/controllers/utils/query_to_json.rs
+++ crates/server/src/controllers/utils/query_to_json.rs
@@ -1,7 +1,6 @@
 use rusqlite::{types::Value as RusqliteValue, Error, Params, Statement};
 use serde_json::{json, Value as JsonValue};
 
-#[allow(dead_code)]
 pub fn query_to_json<P: Params>(mut stmt: Statement, params: P) -> Result<JsonValue, Error> {
     let names = stmt
         .column_names()
@@ -39,5 +38,86 @@ fn _merge(prev: &mut JsonValue, next: &JsonValue) {
         (prev, next) => {
             *prev = next.clone();
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rusqlite::Connection;
+
+    use crate::controllers::utils::bind_to_params::{bind_array_to_params, bind_object_to_params};
+
+    use super::*;
+
+    #[test]
+    fn test_query_to_json_array_params() {
+        let conn = Connection::open_in_memory().unwrap();
+        conn.execute(
+            "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)",
+            [],
+        )
+        .unwrap();
+        conn.execute(
+            "INSERT INTO users (name, age) VALUES ('Alice', 25), ('Bob', 30)",
+            [],
+        )
+        .unwrap();
+
+        let query = "SELECT * FROM users WHERE age > ?";
+        let values: JsonValue = serde_json::from_str("[24]").unwrap();
+
+        let result = if values.is_object() {
+            let (params, updated_query) = bind_object_to_params(values, query.to_owned()).unwrap();
+            let statement = conn.prepare(&updated_query);
+
+            query_to_json(statement.unwrap(), params).unwrap()
+        } else {
+            let params = bind_array_to_params(values);
+            let statement = conn.prepare(query);
+
+            query_to_json(statement.unwrap(), params).unwrap()
+        };
+
+        assert_eq!(result.as_array().unwrap().len(), 2);
+        assert_eq!(result[0]["name"], "Alice");
+        assert_eq!(result[0]["age"], 25);
+        assert_eq!(result[1]["name"], "Bob");
+        assert_eq!(result[1]["age"], 30);
+    }
+
+    #[test]
+    fn test_query_to_json_object_params() {
+        let conn = Connection::open_in_memory().unwrap();
+        conn.execute(
+            "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)",
+            [],
+        )
+        .unwrap();
+        conn.execute(
+            "INSERT INTO users (name, age) VALUES ('Alice', 25), ('Bob', 30)",
+            [],
+        )
+        .unwrap();
+
+        let query = "SELECT * FROM users WHERE age > :age";
+        let values: JsonValue = serde_json::from_str(r#"{":age": 24}"#).unwrap();
+
+        let result = if values.is_object() {
+            let (params, updated_query) = bind_object_to_params(values, query.to_owned()).unwrap();
+            let statement = conn.prepare(&updated_query);
+
+            query_to_json(statement.unwrap(), params).unwrap()
+        } else {
+            let params = bind_array_to_params(values);
+            let statement = conn.prepare(query);
+
+            query_to_json(statement.unwrap(), params).unwrap()
+        };
+
+        assert_eq!(result.as_array().unwrap().len(), 2);
+        assert_eq!(result[0]["name"], "Alice");
+        assert_eq!(result[0]["age"], 25);
+        assert_eq!(result[1]["name"], "Bob");
+        assert_eq!(result[1]["age"], 30);
     }
 }
