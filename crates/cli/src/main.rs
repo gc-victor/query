ca72020d345616b3222b02cb7885afa6962e6db5
Víctor García <gc.victor@gmail.com>
2024-03-22 16:59:22 +0100

    release: version 0.5.3

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -17,7 +17,7 @@ use tracing_subscriber::FmtSubscriber;
 
 #[derive(Parser)]
 #[command(name = "Query")]
-#[command(version = "0.5.2")]
+#[command(version = "0.5.3")]
 #[command(about = "The CLI to manage your Query Server instance", long_about = None)]
 struct Cli {
     #[command(subcommand)]
