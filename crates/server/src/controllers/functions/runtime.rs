8450259c5f4d6213aa3068c61113f3d509a1691c
Víctor García <gc.victor@gmail.com>
2024-03-27 17:24:01 +0100

    feat: add timeout to the runtime

--- crates/server/src/controllers/functions/runtime.rs
+++ crates/server/src/controllers/functions/runtime.rs
@@ -1,33 +1,30 @@
-use std::cell::{OnceCell, RefCell};
+use std::time::Duration;
 
 use rustyscript::{Runtime, RuntimeOptions};
+use tracing::error;
 
 use super::ext::{
     init_argon2::init_argon2, init_handle_response::init_handle_response,
     init_process::init_process, init_sqlite::init_sqlite,
 };
 
-thread_local! {
-    static RUNTIME_CELL: OnceCell<RefCell<Runtime>> = OnceCell::new();
-}
-
 pub fn with_runtime<T, F: FnMut(&mut Runtime) -> T>(mut callback: F) -> T {
-    RUNTIME_CELL.with(|once_lock| {
-        let rt_mut = once_lock.get_or_init(|| {
-            RefCell::new(
-                Runtime::new(RuntimeOptions {
-                    extensions: vec![
-                        init_argon2::init_ops_and_esm(),
-                        init_handle_response::init_ops_and_esm(),
-                        init_process::init_ops_and_esm(),
-                        init_sqlite::init_ops_and_esm(),
-                    ],
-                    ..Default::default()
-                })
-                .expect("could not create the runtime"),
-            )
-        });
-        let mut runtime = rt_mut.borrow_mut();
-        callback(&mut runtime)
-    })
+    let mut runtime = match Runtime::new(RuntimeOptions {
+        extensions: vec![
+            init_argon2::init_ops_and_esm(),
+            init_handle_response::init_ops_and_esm(),
+            init_process::init_ops_and_esm(),
+            init_sqlite::init_ops_and_esm(),
+        ],
+        timeout: Duration::from_millis(5000),
+        ..Default::default()
+    }) {
+        Ok(runtime) => runtime,
+        Err(err) => {
+            error!("Failed to create runtime: {:?}", err);
+            std::process::exit(0);
+        }
+    };
+
+    callback(&mut runtime)
 }
