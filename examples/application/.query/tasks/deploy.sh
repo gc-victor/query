5d495672c4a6fa83b39d44eb74aad7ed114c77b3
Víctor García <gc.victor@gmail.com>
2024-05-15 15:11:11 +0200

    feat(example): update application tasks

--- examples/application/.query/tasks/deploy.sh
+++ examples/application/.query/tasks/deploy.sh
@@ -4,29 +4,29 @@ export PROD=true
 
 deploy_bundle() {
     start=$(date +%s%N)
-    pnpm query task bundle
+    node_modules/.bin/query task bundle
     end=$(date +%s%N)
     echo "Bundled: $(((end - start) / 1000000))ms"
 }
 
 deploy_tailwind() {
     start=$(date +%s%N)
-    pnpm query task tailwind
+    node_modules/.bin/query task tailwind
     end=$(date +%s%N)
     echo "Styles created: $(((end - start) / 1000000))ms"
 }
 
 deploy_asset() {
     start=$(date +%s%N)
-    pnpm query asset dist &
-    pnpm query asset public
+    node_modules/.bin/query asset dist &
+    node_modules/.bin/query asset public
     end=$(date +%s%N)
     echo "Assets updated: $(((end - start) / 1000000))ms"
 }
 
 deploy_function() {
     start=$(date +%s%N)
-    pnpm query function
+    node_modules/.bin/query function
     end=$(date +%s%N)
     echo "Functions updated: $(((end - start) / 1000000))ms"
 }
