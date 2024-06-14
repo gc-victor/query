2be49b6cec9924606bc85ebc6f568429f0d2db54
Víctor García <gc.victor@gmail.com>
2024-06-14 16:59:41 +0200

    fix(server): multipart form-data fields with same name

--- hurl/function/multipart.js
+++ hurl/function/multipart.js
@@ -5,8 +5,10 @@ const funFunFunction = `globalThis.___handleRequest = async (req) => {
         const password = formData.get("password");
         const field = formData.get("field");
         const duplicated = formData.getAll("duplicated");
+        const arr = formData.getAll("arr");
+        const json = formData.getAll("json");
         const list = formData.getAll("list");
-        const str = \`email: \${email} | password: \${password} | file: \${field.name} - \${field.type} - \${field.size} | duplicated: \${duplicated}\ | duplicatedArray: \${Array.isArray(duplicated)}\ | list: \${list} | listArray: \${Array.isArray(list)}\`;
+        const str = \`email: \${email} | password: \${password} | file: \${field.name} - \${field.type} - \${field.size} | duplicated: \${duplicated}\ | duplicatedArray: \${Array.isArray(duplicated)}\ | arr: \${arr}\ | arrArray: \${Array.isArray(arr)}\ | json: \${json}\ | jsonArray: \${Array.isArray(json)}\ | list: \${list} | listArray: \${Array.isArray(list)}\`;
 
         return new Response(str, {
             status: 200,
