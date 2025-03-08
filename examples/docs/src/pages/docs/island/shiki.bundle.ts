de6952ae5a06f267af8a9a19137c08ce744cc30f
Víctor García <gc.victor@gmail.com>
2025-03-08 19:05:05 +0100

    fix(example): remove the duplicate language

--- examples/docs/src/pages/docs/island/shiki.bundle.ts
+++ examples/docs/src/pages/docs/island/shiki.bundle.ts
@@ -17,17 +17,10 @@ type BundledLanguage =
   | 'sh'
   | 'shell'
   | 'zsh'
-  | 'shellscript'
-  | 'bash'
-  | 'sh'
-  | 'shell'
-  | 'zsh'
   | 'javascript'
   | 'js'
   | 'jsx'
   | 'tsx'
-  | 'javascript'
-  | 'js'
   | 'typescript'
   | 'ts'
   | 'html'
@@ -49,17 +42,10 @@ const bundledLanguages = {
   sh: () => import('@shikijs/langs/shellscript'),
   shell: () => import('@shikijs/langs/shellscript'),
   zsh: () => import('@shikijs/langs/shellscript'),
-  shellscript: () => import('@shikijs/langs/shellscript'),
-  bash: () => import('@shikijs/langs/shellscript'),
-  sh: () => import('@shikijs/langs/shellscript'),
-  shell: () => import('@shikijs/langs/shellscript'),
-  zsh: () => import('@shikijs/langs/shellscript'),
   javascript: () => import('@shikijs/langs/javascript'),
   js: () => import('@shikijs/langs/javascript'),
   jsx: () => import('@shikijs/langs/jsx'),
   tsx: () => import('@shikijs/langs/tsx'),
-  javascript: () => import('@shikijs/langs/javascript'),
-  js: () => import('@shikijs/langs/javascript'),
   typescript: () => import('@shikijs/langs/typescript'),
   ts: () => import('@shikijs/langs/typescript'),
   html: () => import('@shikijs/langs/html'),
@@ -88,12 +74,10 @@ const createHighlighter = /* @__PURE__ */ createdBundledHighlighter<
 
 const {
   codeToHtml,
-  codeToHast,
   codeToTokensBase,
   codeToTokens,
   codeToTokensWithThemes,
   getSingletonHighlighter,
-  getLastGrammarState,
 } = /* @__PURE__ */ createSingletonShorthands<BundledLanguage, BundledTheme>(
   createHighlighter,
 )
@@ -101,13 +85,11 @@ const {
 export {
   bundledLanguages,
   bundledThemes,
-  codeToHast,
   codeToHtml,
   codeToTokens,
   codeToTokensBase,
   codeToTokensWithThemes,
   createHighlighter,
-  getLastGrammarState,
   getSingletonHighlighter,
 }
 export type { BundledLanguage, BundledTheme, Highlighter }
