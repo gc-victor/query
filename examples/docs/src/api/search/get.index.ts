d3b921659ada18ac837ed9aad16b34e67cf234b4
Víctor García <gc.victor@gmail.com>
2025-03-08 18:51:31 +0100

    fix(example): sanitize search query

--- examples/docs/src/api/search/get.index.ts
+++ examples/docs/src/api/search/get.index.ts
@@ -22,35 +22,19 @@ async function handleSearchRequest(req: Request): Promise<Response> {
     const url = new URL(req.url);
     const query = url.searchParams.get("q") || "";
 
-    if (!query.trim()) {
-        const emptyResponse: SearchResponse = {
-            results: [],
-        };
+    const words = query
+        .replace(/[\\"':*^$~()\[\]{}<>!|&+\-]/g, " ")
+        .replace(/\s+/g, " ")
+        .trim()
+        .toLowerCase()
+        .split(/\s+/)
+        .filter((word) => word.length > 0 && !["or", "and", "not", "near", "match"].includes(word));
 
-        return new Response(JSON.stringify(emptyResponse), {
-            status: 200,
-            headers: IS_DEVELOPMENT
-                ? {
-                      "Content-Type": "application/json",
-                  }
-                : {
-                      "Content-Type": "application/json",
-                      "Cache-Control": "max-age=3600",
-                      "Query-Cache-Control": "max-age=3600000",
-                  },
-        });
+    if (words.length === 0) {
+        return emptyQueryResponse();
     }
 
-    const sanitizedQuery = query.replace(/[\"|*]/g, " ").trim();
-    const words = sanitizedQuery.split(/\s+/).filter((word) => word.length > 0);
-
-    let searchQuery: string;
-    if (words.length > 1) {
-        searchQuery = `${sanitizedQuery} OR ${words.map((word) => `${word}`).join(" OR ")}`;
-    } else {
-        searchQuery = `${sanitizedQuery}* OR ${sanitizedQuery}`;
-    }
-
+    const searchQuery = words.length === 1 ? `${words[0]} OR ${words[0]}*` : `${words.join(" ")} OR ${words.join(" OR ")}`;
     const db = new Database("query_asset.sql");
     const results = db.query<SearchResult>(
         `SELECT title, path, snippet(docs_search, 2, '<strong>', '</strong>', '', 20) as section FROM docs_search WHERE docs_search MATCH ? ORDER BY rank LIMIT 5`,
@@ -71,6 +55,25 @@ async function handleSearchRequest(req: Request): Promise<Response> {
                   "Content-Type": "application/json",
                   "Cache-Control": "public, max-age=300",
                   "Query-Cache-Control": "max-age=300000",
+              },
+    });
+}
+
+function emptyQueryResponse() {
+    const emptyResponse: SearchResponse = {
+        results: [],
+    };
+
+    return new Response(JSON.stringify(emptyResponse), {
+        status: 200,
+        headers: IS_DEVELOPMENT
+            ? {
+                  "Content-Type": "application/json",
+              }
+            : {
+                  "Content-Type": "application/json",
+                  "Cache-Control": "max-age=3600",
+                  "Query-Cache-Control": "max-age=3600000",
               },
     });
 }
